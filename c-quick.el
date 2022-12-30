17a06fed2ab258345fdb6529c02af19b1f897ca1
JavaCommons Technologies <javacommons@gmail.com>
2022-12-30 18:35:28 +0900

    .

--- c-quick.el
+++ c-quick.el
@@ -1,5 +1,5 @@
 ;; -*- coding: utf-8 -*-
-(setq *c-quick-version* "v2.7.4")
+(setq *c-quick-version* "v2.7.5")
 ;;; c-quick.el --- Intelligent Cursor Movement for GNU Emacs
 ;;
 ;; Copyright (C) 1993-2022 JavaCommons Technologies
